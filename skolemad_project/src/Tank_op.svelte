4fc01460012cb8f57aacba25d413b8ee85242f3d
Soph0298 <Soph0298@stud.kea.dk>
2022-03-21 10:44:48 +0100

    vatriable

--- skolemad_project/src/Tank_op.svelte
+++ skolemad_project/src/Tank_op.svelte
@@ -1,8 +1,14 @@
+<script>
+  import Primary_button from "./Primary_button.svelte";
+</script>
+
 <body>
   <h1>h1 denne overskrift test</h1>
   <h2>h2 denne overskrift test</h2>
   <h3>h1 denne overskrift test</h3>
   <h4>h1 denne overskrift test</h4>
+  <p>h1 denne overskrift test</p>
   <div>h1 denne overskrift test</div>
-  <div>h1 denne overskrift test</div>
+
+  <Primary_button>Klik mig nu</Primary_button>
 </body>
