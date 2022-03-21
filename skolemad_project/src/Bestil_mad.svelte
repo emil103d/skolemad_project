8b450550b2028c1f3c57195a8c2d401c8b1e7a12
emilie carlsen <emil103d@stud.kea.dk>
2022-03-21 09:46:57 +0100

    billede

---
+++ skolemad_project/src/Bestil_mad.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<main>
+  <img src="img/madder.png" alt="" />
+  <h2>Månedens mad</h2>
+  <div class="underoverskrift">
+    <svg
+      width="19"
+      height="25"
+      viewBox="0 0 19 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M18 24L1 12.5L18 1" stroke="black" />
+    </svg>
+    <h3>Frokoster for februar</h3>
+    <svg
+      width="19"
+      height="25"
+      viewBox="0 0 19 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1 1L18 12.5L1 24" stroke="black" />
+    </svg>
+  </div>
+</main>
+
+<style>
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+</style>
