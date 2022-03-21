d3e1020210fa4d1d95cd86c79ac2558b60840484
Soph0298 <Soph0298@stud.kea.dk>
2022-03-21 11:19:40 +0100

    margin

--- skolemad_project/src/Components/Footer.svelte
+++ skolemad_project/src/Components/Footer.svelte
@@ -1,4 +1,41 @@
-<footer />
+<footer>
+  <ul>
+    <li><img src="" alt="" /></li>
+    <li>Hejrevej Alle 25</li>
+    <li>2300 København S</li>
+    <li>CVR: 1234 5678</li>
+
+    <ul>
+      <li><h3>Om skolemad.dk</h3></li>
+      <li>Hvad er skolemad.dk</li>
+      <li>Hvad er skolemad.dk</li>
+      <li>Hvad er skolemad.dk</li>
+    </ul>
+
+    <ul>
+      <li><h3>Kundeservice</h3></li>
+      <li>Hvad er skolemad.dk</li>
+      <li>Hvad er skolemad.dk</li>
+      <li>Hvad er skolemad.dk</li>
+    </ul>
+
+    <ul>
+      <li><h3>Følg med på...</h3></li>
+      <li><img src="" alt="" /></li>
+      <li><img src="" alt="" /></li>
+      <li><img src="" alt="" /></li>
+    </ul>
+  </ul>
+</footer>
 
 <style>
+  footer {
+    list-style: none;
+    background-color: var(--secondary);
+    color: var(--sort);
+  }
+
+  ul {
+    display: flex;
+  }
 </style>
