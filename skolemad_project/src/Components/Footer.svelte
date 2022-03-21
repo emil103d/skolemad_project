b57ca3074bad5268d5ca11773c3a8171242d26f3
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-03-21 15:59:21 +0100

    Footer indsat (igen)

--- skolemad_project/src/Components/Footer.svelte
+++ skolemad_project/src/Components/Footer.svelte
@@ -1,28 +1,64 @@
 <script>
+
 </script>
 
+<footer>
+    <div>
+        <h3>SERVICES</h3>
+        <ul>
+            <li>Branding</li>
+            <li>Design</li>
+            <li>Marketing</li>
+            <li>Advertisement</li>
+        </ul>
+    </div>
+    
+    <div>
+        <h3>COMPANY</h3>
+        <ul>
+            <li>About us</li>
+            <li>Contact</li>
+            <li>Jobs</li>
+            <li>Press kit</li>
+        </ul>
+    </div>
+
+    <div>
+        <h3>LEGAL</h3>
+        <ul>
+            <li>Terms of use</li>
+            <li>Privacy policy</li>
+            <li>Cookie policy</li>
+        </ul>
+    </div>
+</footer>
+
 <style>
-  footer {
-    background-color: var(--secondary);
-    color: var(--sort);
-    display: flex;
-  }
 
-  @media (min-width: 810px) {
     footer {
-      justify-content: space-between;
-      padding: 4rem 5rem;
-      font-weight: 300;
-      line-height: 2rem;
+        background-color: #3E4450;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+        justify-items: center;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
     }
 
-    .someikoner svg {
-      width: 40px;
+    h3 {
+        font-size: 0.7rem;
+        color: gray
     }
 
-    .someikoner {
-      display: flex;
-      gap: 1.4rem;
+    ul li {
+        font-size: 0.6rem;
+        color: white;
+        list-style: none;
+        list-style-position: outside;
+        line-height: 200%;
     }
-  }
+
+    ul {
+        padding: 0;
+    }
+
 </style>
