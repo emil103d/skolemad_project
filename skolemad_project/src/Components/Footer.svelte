3126e5feb1ed617e621e4dbef97186fc4fde6cee
Soph0298 <Soph0298@stud.kea.dk>
2022-03-21 15:50:30 +0100

    footer

--- skolemad_project/src/Components/Footer.svelte
+++ skolemad_project/src/Components/Footer.svelte
@@ -105,19 +105,23 @@
     background-color: var(--secondary);
     color: var(--sort);
     display: flex;
-    display: flex;
-    justify-content: space-between;
-    padding: 4rem 5rem;
-    font-weight: 300;
-    line-height: 2rem;
   }
 
-  .someikoner svg {
-    width: 40px;
-  }
+  @media (min-width: 810px) {
+    footer {
+      justify-content: space-between;
+      padding: 4rem 5rem;
+      font-weight: 300;
+      line-height: 2rem;
+    }
 
-  .someikoner {
-    display: flex;
-    gap: 1.4rem;
+    .someikoner svg {
+      width: 40px;
+    }
+
+    .someikoner {
+      display: flex;
+      gap: 1.4rem;
+    }
   }
 </style>
