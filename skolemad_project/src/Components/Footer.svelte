3b3ead93d550c0fbebb327474f9d381ed3200960
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-03-21 15:51:25 +0100

    Footer lavet

--- skolemad_project/src/Components/Footer.svelte
+++ skolemad_project/src/Components/Footer.svelte
@@ -1,41 +1,64 @@
+<script>
+
+</script>
+
 <footer>
-  <ul>
-    <li><img src="" alt="" /></li>
-    <li>Hejrevej Alle 25</li>
-    <li>2300 København S</li>
-    <li>CVR: 1234 5678</li>
+    <div>
+        <h3>SERVICES</h3>
+        <ul>
+            <li>Branding</li>
+            <li>Design</li>
+            <li>Marketing</li>
+            <li>Advertisement</li>
+        </ul>
+    </div>
+    
+    <div>
+        <h3>COMPANY</h3>
+        <ul>
+            <li>About us</li>
+            <li>Contact</li>
+            <li>Jobs</li>
+            <li>Press kit</li>
+        </ul>
+    </div>
 
-    <ul>
-      <li><h3>Om skolemad.dk</h3></li>
-      <li>Hvad er skolemad.dk</li>
-      <li>Hvad er skolemad.dk</li>
-      <li>Hvad er skolemad.dk</li>
-    </ul>
-
-    <ul>
-      <li><h3>Kundeservice</h3></li>
-      <li>Hvad er skolemad.dk</li>
-      <li>Hvad er skolemad.dk</li>
-      <li>Hvad er skolemad.dk</li>
-    </ul>
-
-    <ul>
-      <li><h3>Følg med på...</h3></li>
-      <li><img src="" alt="" /></li>
-      <li><img src="" alt="" /></li>
-      <li><img src="" alt="" /></li>
-    </ul>
-  </ul>
+    <div>
+        <h3>LEGAL</h3>
+        <ul>
+            <li>Terms of use</li>
+            <li>Privacy policy</li>
+            <li>Cookie policy</li>
+        </ul>
+    </div>
 </footer>
 
 <style>
-  footer {
-    list-style: none;
-    background-color: var(--secondary);
-    color: var(--sort);
-  }
 
-  ul {
-    display: flex;
-  }
+    footer {
+        background-color: #3E4450;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+        justify-items: center;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+    }
+
+    h3 {
+        font-size: 0.7rem;
+        color: gray
+    }
+
+    ul li {
+        font-size: 0.6rem;
+        color: white;
+        list-style: none;
+        list-style-position: outside;
+        line-height: 200%;
+    }
+
+    ul {
+        padding: 0;
+    }
+
 </style>
