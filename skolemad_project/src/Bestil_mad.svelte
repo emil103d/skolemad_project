4f4fd354fef317b4fe25633972f429e0a981c5a9
emilie carlsen <emil103d@stud.kea.dk>
2022-03-21 10:44:50 +0100

    ...

--- skolemad_project/src/Bestil_mad.svelte
+++ skolemad_project/src/Bestil_mad.svelte
@@ -1,10 +1,11 @@
 <script>
+  import Uge_menu from "./Components/Uge_menu.svelte";
 </script>
 
 <main>
   <img src="img/madder.png" alt="" />
   <h2>Månedens mad</h2>
-  <div class="frokost menuer">
+  <div class="frokost_menuer">
     <div class="underoverskrift">
       <svg
         width="19"
@@ -26,10 +27,25 @@
         <path d="M1 1L18 12.5L1 24" stroke="black" />
       </svg>
     </div>
+    <div class="menuer">
+      <Uge_menu
+        uge="5"
+        dato="9. feb - 12.feb"
+        tema_categori="Jorden Rundt"
+        man_ret="Pitabrød m. Kebab"
+        tirs_ret="Sweet potato suppe"
+        ons_ret="Gryderet med kartofler i karry sauce"
+        tors_ret="Club Sandwich"
+        fre_ret="Pasta med Kødsovs"
+      />
+    </div>
   </div>
 </main>
 
 <style>
+  .frokost_menuer {
+    background-color: beige;
+  }
   img {
     width: 100%;
     height: 16rem;
---
+++ skolemad_project/src/Components/Uge_menu.svelte
@@ -0,0 +1,56 @@
+<script>
+  export let uge;
+  export let dato;
+  export let tema_categori;
+  export let man_ret;
+  export let tirs_ret;
+  export let ons_ret;
+  export let tors_ret;
+  export let fre_ret;
+</script>
+
+<article class="container">
+  <div class="title">
+    <h3>Uge{uge}</h3>
+    <p>{dato}</p>
+  </div>
+  <img src="img/ret1.png" alt="" />
+  <div>
+    <h4>Tema: {tema_categori}</h4>
+    <p>Manadag: {man_ret}</p>
+    <p>Tirsdag: {tirs_ret}</p>
+    <p>Onsdag: {ons_ret}</p>
+    <p>Torsdag: {tors_ret}</p>
+    <p>Fredag: {fre_ret}</p>
+  </div>
+</article>
+
+<style>
+  .container {
+    background: white;
+    border-radius: var(--border-radius-sm);
+    width: 20rem;
+    padding-top: 1rem;
+  }
+
+  .title {
+    text-align: center;
+  }
+
+  div {
+    margin: 1rem;
+  }
+
+  .title p {
+    margin-bottom: 1rem;
+  }
+
+  h4 {
+    margin-block: 1rem;
+  }
+
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
+</style>
